1233879b71f155ef1a847e9892fe30e68b9d088e
S-troup10 <simontroup27@gmail.com>
2026-01-29 17:09:18 +0400

    Add files via upload

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/dynk-CRM/sw.js', { scope: '/dynk-CRM/' })})}
